7b29cff24190cd6b2f4bc82e52163bb2a5c395d2
Hendi Santika <hendisantika@yahoo.co.id>
2022-10-23 15:56:10 +0700

    #5 - Add more method in EmployeeRepository :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/repository/EmployeeRepository.java
+++ src/main/java/com/hendisantika/repository/EmployeeRepository.java
@@ -1,8 +1,14 @@
 package com.hendisantika.repository;
 
 import com.hendisantika.entity.Employee;
+import com.hendisantika.mapper.EmployeeMapper;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +22,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    List<Employee> findByName(String name);
+
+    List<Employee> findByEmployeeId(String name);
+
+    @Query(value = "SELECT id , name, salary, 'true' as is_create FROM employees where name"
+            + " like :empName and salary >= :empSalary", rowMapperClass = EmployeeMapper.class)
+    List<Employee> findByNameAndSalary(@Param("empName") String name, @Param("empSalary") Double salary);
+
+
+    @Modifying
+    @Query(value = "update employees set salary = :salary where id = :id ")
+    int updateSalary(Double salary, Long id);
 }
