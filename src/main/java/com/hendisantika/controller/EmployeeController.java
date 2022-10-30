6ce060a6ee6928409c88aaa087ad5b25d349f743
Hendi Santika <hendisantika@yahoo.co.id>
2022-10-30 19:55:28 +0700

    #21 - Add findById in EmployeeController :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import com.hendisantika.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,11 @@ public class EmployeeController {
     @GetMapping("/count")
     public long countEmp() {
         return employeeService.count();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id, @RequestHeader("accept-language") String acceptLanguage) {
+        log.info(" Accept Language is " + acceptLanguage);
+        return ResponseEntity.ok(employeeService.findById(id));
     }
 }
