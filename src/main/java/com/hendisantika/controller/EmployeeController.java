b166fc3e6fe3cd0023a034b7a0f4b61e1578f316
Hendi Santika <hendisantika@yahoo.co.id>
2022-10-31 09:59:00 +0700

    #24 - Add updateEmp in EmployeeController :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,4 +54,8 @@ public class EmployeeController {
         return employeeService.insert(emp);
     }
 
+    @PutMapping("")
+    public Employee updateEmp(@RequestBody Employee emp) {
+        return employeeService.update(emp);
+    }
 }
