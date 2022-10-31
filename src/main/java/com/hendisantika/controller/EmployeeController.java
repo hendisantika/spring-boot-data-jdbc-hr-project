99cbf1ff75caeadbb5978072c03b476d2cfe3c48
Hendi Santika <hendisantika@yahoo.co.id>
2022-10-31 09:58:44 +0700

    #23 - Add addEmp in EmployeeController :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,4 +47,10 @@ public class EmployeeController {
     public Iterable<Employee> findAll() {
         return employeeService.findAll();
     }
+
+    @PostMapping("")
+    public Employee addEmp(@RequestBody Employee emp) {
+        return employeeService.insert(emp);
+    }
+
 }
